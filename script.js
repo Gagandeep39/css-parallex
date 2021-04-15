ab3c0dce78fafd05aa8440b313f689c12e302c71
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-04-16 00:58:50 +0530

    Added response to scrolling for other element

--- script.js
+++ script.js
@@ -8,5 +8,17 @@ let btn = document.querySelector('.btn');
 window.addEventListener('scroll', function () {
   let value = window.scrollY;
   // Shift stars to right on scroll
-  stars.style.left = `${value}px`;
+  stars.style.left = `${value * 0.25}px`;
+  // Shift Moon below
+  moon.style.top = `${value * 1.05}px`;
+  // Shift Back Mountain below
+  mountains_behind.style.top = `${value * 0.5}px`;
+  // Default scroll behavior
+  mountains_front.style.top = `${value * 0}px`;
+  // Moves title text from left to right
+  text.style.marginRight = `${value * 4}px`;
+  // Moves title in a slanted manner
+  text.style.marginTop = `${value * 1.5}px`;
+  // Moves Explroe button down
+  btn.style.marginTop = `${value * 1.5}px`;
 });
